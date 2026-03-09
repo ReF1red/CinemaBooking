2e374de9a685c8d8c056cbba471badad9bf885d2
dmvlk <dimvolkov2006@gmail.com>
2026-03-09 22:57:41 +0300

    fix: заменил драйвера БД на pg8000

--- app/database.py
+++ app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {})
+DATABASE_URL = settings.DATABASE_URL.replace("postgresql://", "postgresql+pg8000://")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
